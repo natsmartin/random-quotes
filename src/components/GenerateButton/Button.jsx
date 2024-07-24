75060363fc9f2f758cc8fc88f35186674949b65b
natsmartin <natsmartin95@gmail.com>
2024-07-25 04:28:18 +0800

    add useState

--- src/components/GenerateButton/Button.jsx
+++ src/components/GenerateButton/Button.jsx
@@ -1,13 +1,27 @@
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchTodos } from '../../redux/slice/todo'
 import './Button.css'
 
 function GenerateButton() {
+
   const dispatch = useDispatch()
+  let initialCount = 0
+  const [count, setCount] = useState(initialCount)
+
+  const onCLick = () => {
+    dispatch(fetchTodos())
+    setCount(count + 1)
+  }
 
   return (
     <div className="generate-button-container">
-      <button className="generate-button" onClick={() => dispatch(fetchTodos())} >Generate</button>
+      <div>
+        <p>Request count: {count}</p>
+      </div>
+      <div>
+        <button className="generate-button" onClick={() => onCLick()} >Generate</button>
+      </div>
     </div>
   )
 }
